27345367062ff84ce04358352b7a812905f67cf2
Thendow <163464666+Thendow@users.noreply.github.com>
2024-03-29 01:24:45 +0200

    updated Javascript

--- src/index.js
+++ src/index.js
@@ -1,59 +1,177 @@
-const A = document.getElementById("task-area");
-const B = document.getElementById("single-task");
-const C = document.getElementById("tasks");
+let firstName = prompt("What is your first name?");
+if (firstName.length > 0) {
+  alert(
+    "Welcome, " +
+      firstName +
+      ". to our innovative To-Do List App, where productivity meets simplicity."
+  );
+} else {
+  alert("Too bad for you🤕🤒😷");
+}
+const todoValue = document.getElementById("todoText");
+const todoAlert = document.getElementById("Alert");
+const listItems = document.getElementById("list-items");
+const addUpdate = document.getElementById("AddUpdateClick");
 
-function addTask(event) {
-  event.preventDefault();
-  if (B.value === "") return;
-  const task = createTask(B.value);
-  C.appendChild(task);
-  B.value = "";
+let todo = JSON.parse(localStorage.getItem("todo-list"));
+if (!todo) {
+  todo = [];
 }
 
-function createTask(taskName) {
-  const task = document.createElement("li");
-  task.classList.add("task");
-  task.innerHTML = `
-        <input type="checkbox">
-        <label>${taskName}</label>
-        <span class="delete">×</span>
-    `;
-
-  const deleteButton = task.querySelector(".delete");
-  deleteButton.addEventListener("click", deleteTask);
-
-  return task;
-}
-
-function deleteTask(event) {
-  event.target.parentElement.remove();
-}
-
-A.addEventListener("submit", addTask);
-B.addEventListener("submit", addTask);
-C.addEventListener("submit", addTask);
-
-let tasks = Object.keys(localStorage);
-tasks = tasks.sort();
-
-for (let key of tasks) {
-  let classValue = "";
-  let value = localStorage.getItem(key);
-  let taskInnerDiv = document.createElement("div");
-  taskInnerDiv.classList.add("task");
-  taskInnerDiv.setAttribute("id", key);
-  taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
-  //localstorage would store boolean as string so we parse it to boolean back
-  let editButton = document.createElement("button");
-  editButton.classList.add("edit");
-  editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
-  if (!JSON.parse(value)) {
-    editButton.style.visibility = "visible";
+function CreateToDoItems() {
+  if (todoValue.value === "") {
+    todoAlert.innerText = "Please enter your TO-DO item...";
+    todoValue.focus();
   } else {
-    editButton.style.visibility = "hidden";
-    taskInnerDiv.classList.add("completed");
+    let IsPresent = false;
+    todo.forEach((element) => {
+      if (element.item == todoValue.value) {
+        IsPresent = true;
+      }
+    });
+
+    if (IsPresent) {
+      setAlertMessage("This item is already present in the list!");
+      return;
+    }
+
+    let li = document.createElement("li");
+    const todoItems = `<div title="Hit Double Click and Complete" ondblclick="CompletedToDoItems(this)">${todoValue.value}</div><div>
+                    <img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="images/pencil.png" />
+                    <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="images/delete.png" /></div></div>`;
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+
+    if (!todo) {
+      todo = [];
+    }
+    let itemList = { item: todoValue.value, status: false };
+    todo.push(itemList);
+    setLocalStorage();
   }
-  taskInnerDiv.appendChild(editButton);
-  taskInnerDiv.innerHTML += `<button class="delete"><i class="fa-solid fa-trash"></i></button>`;
-  tasksDiv.appendChild(taskInnerDiv);
+  todoValue.value = "";
+  setAlertMessage("TO-DO item is created successfully!");
+}
+
+function ReadToDoItems() {
+  todo.forEach((element) => {
+    let li = document.createElement("li");
+    let style = "";
+    if (element.status) {
+      style = "style='text-decoration: line-through'";
+    }
+    const todoItems = `<div ${style} title="Hit Double Click and Complete" ondblclick="CompletedToDoItems(this)">${
+      element.item
+    }
+    ${
+      style === ""
+        ? ""
+        : '<img class="todo-controls" src="images/check-mark.png" />'
+    }</div><div>
+    ${
+      style === ""
+        ? '<img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="images/pencil.png" />'
+        : ""
+    }
+    <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="images/delete.png" /></div></div>`;
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+  });
+}
+ReadToDoItems();
+
+function UpdateToDoItems(e) {
+  if (
+    e.parentElement.parentElement.querySelector("div").style.textDecoration ===
+    ""
+  ) {
+    todoValue.value =
+      e.parentElement.parentElement.querySelector("div").innerText;
+    updateText = e.parentElement.parentElement.querySelector("div");
+    addUpdate.setAttribute("onclick", "UpdateOnSelectionItems()");
+    addUpdate.setAttribute("src", "images/refresh.png");
+    todoValue.focus();
+  }
+}
+
+function UpdateOnSelectionItems() {
+  let IsPresent = false;
+  todo.forEach((element) => {
+    if (element.item == todoValue.value) {
+      IsPresent = true;
+    }
+  });
+
+  if (IsPresent) {
+    setAlertMessage("This item is already present in the list!");
+    return;
+  }
+
+  todo.forEach((element) => {
+    if (element.item == updateText.innerText.trim()) {
+      element.item = todoValue.value;
+    }
+  });
+  setLocalStorage();
+
+  updateText.innerText = todoValue.value;
+  addUpdate.setAttribute("onclick", "CreateToDoItems()");
+  addUpdate.setAttribute("src", "images/plus.png");
+  todoValue.value = "";
+  setAlertMessage("TO-DO item is created successfully!");
+}
+
+function DeleteToDoItems(e) {
+  let deleteValue =
+    e.parentElement.parentElement.querySelector("div").innerText;
+
+  if (confirm(`Are you sure you want to delete this? ${deleteValue}!`)) {
+    e.parentElement.parentElement.setAttribute("class", "deleted-item");
+    todoValue.focus();
+
+    todo.forEach((element) => {
+      if (element.item == deleteValue.trim()) {
+        todo.splice(element, 1);
+      }
+    });
+
+    setTimeout(() => {
+      e.parentElement.parentElement.remove();
+    }, 1000);
+
+    setLocalStorage();
+  }
+}
+
+function CompletedToDoItems(e) {
+  if (e.parentElement.querySelector("div").style.textDecoration === "") {
+    const img = document.createElement("img");
+    img.src = "images/check-mark.png";
+    img.className = "todo-controls";
+    e.parentElement.querySelector("div").style.textDecoration = "line-through";
+    e.parentElement.querySelector("div").appendChild(img);
+    e.parentElement.querySelector("img.edit").remove();
+
+    todo.forEach((element) => {
+      if (
+        e.parentElement.querySelector("div").innerText.trim() == element.item
+      ) {
+        element.status = true;
+      }
+    });
+    setLocalStorage();
+    setAlertMessage("TO-DO item Completed Successfully!");
+  }
+}
+
+function setLocalStorage() {
+  localStorage.setItem("todo-list", JSON.stringify(todo));
+}
+
+function setAlertMessage(message) {
+  todoAlert.removeAttribute("class");
+  todoAlert.innerText = message;
+  setTimeout(() => {
+    todoAlert.classList.add("toggleMe");
+  }, 1000);
 }
